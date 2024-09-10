4465578e6c51fca1fb8541e6e209c76ff22ed6d5
João Victor Ribeiro <jvictor.ribeiro03@gmail.com>
2024-09-09 22:33:23 -0300

    adding backgroundcolor according to type

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -2,6 +2,28 @@ import React, {useState} from "react";
 import axios from "axios";
 import './Style.css'
 
+// Mapeamento de cores para cada tipo de pokemon
+const typeColors = {
+  fire: '#F08030',
+  water: '#6890F0',
+  grass: '#78c850',
+  electric: '#F8D030',
+  ice: '#98D8D8',
+  fighting: '#C03028',
+  poison: '#A040A0',
+  ground: '#E0C068',
+  flying: '#A890F0',
+  psychic: '#F85888',
+  bug: '#A8B820',
+  rock: '#B8A038',
+  ghost: '#705898',
+  dragon: '#7038F8',
+  dark: '#705848',
+  steel: '#B8B8D0',
+  fairy: '#EE99AC',
+  normal: '#A8A878',
+};
+
 function PokemonList() {
   const [pokemon, setPokemon] = useState(null);
   const [search, setSearch] = useState('');
@@ -19,9 +41,23 @@ function PokemonList() {
     }
   };
 
+  const getBackgroundColor = () => {
+    if(pokemon) {
+      const primaryType = pokemon.types[0].type.name;
+      return typeColors[primaryType] || '#fff';
+    }
+     return '#fff';
+  };
+
 
   return (
-    <div>
+    <div
+      style={{
+        backgroundColor: getBackgroundColor(),
+        padding: '20px',
+        borderRadius: '10px',
+      }}
+    >
       <h1>Pokédex</h1>
       <form onSubmit={handleSearch}>
         <input 
@@ -30,8 +66,18 @@ function PokemonList() {
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           placeholder="Digite o nome de um Pokémon!"
+          style={{padding: '10px', marginRight: '10px', borderRadius: '5px', border: '1px solid black'}}
         />
-        <button type="submit">Buscar</button>
+        <button 
+          type="submit"
+          style={{padding: '10px', 
+            backgroundColor: "lightgreen", 
+            border: 'none', 
+            borderRadius: '5px',
+          }}
+        >
+          Buscar
+        </button>
       </form>
       
       {error && <p style={{ color: 'red' }}>{error}</p>}
